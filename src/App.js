c68d9b08bbf6ba2bd423be59ca8218a88c5e398a
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-06-22 21:06:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import ListItems from "./ListItems";
+import "./styles.css";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const addNewItem = () => {
+    items.push(newItem);
+    setItems([...items]);
+    setNewItem("");
+  };
+
+  const getData = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const deleteHandler = (itemIdx) => {
+    items.splice(itemIdx, 1);
+    setItems([...items]);
+  };
+
+  const editHandler = (editTask, itemIdx) => {
+    items[itemIdx] = editTask;
+    setItems([...items]);
+  };
+
+  return (
+    <div id="main">
+      <textarea
+        id="task"
+        onChange={getData}
+        placeholder="New Task"
+        value={newItem}
+      ></textarea>
+      <br />
+      <button
+        id="btn"
+        onClick={addNewItem}
+        disabled={newItem.trim().length === 0}
+      >
+        Add Task
+      </button>
+
+      {items.map((item, idx) => (
+        <ListItems
+          item={item}
+          key={`${item}_${idx}`}
+          idx={idx}
+          editHandler={editHandler}
+          deleteHandler={deleteHandler}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
